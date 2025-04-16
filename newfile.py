af67fc5611cd2ab76bd6895f03cfa1fcf2b84366
manisharifizadeh <manisharifizade84@gmail.com>
2025-04-16 12:50:40 +0330

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,12 @@
+# I got input from the user
+
+country = input(''Where is your favorite country?,'')
+
+#Surveying a country and printing a city fom it
+
+if country == ''Iran'':
+	print(''Tehran.'')
+elif country == ''England'':
+	print(''London.'')
+else:
+	print(''Sorry, I dont have information about this country..'')	
