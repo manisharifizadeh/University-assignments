7fa14592f550387e005d7573e3fba30e3a04717b
mani <manisharifizade84@gmail.com>
2025-04-16 13:42:21 +0330

    Add files via upload

---
+++ Uni.py
@@ -0,0 +1,13 @@
+city_Name = input("where is your city : ")
+if city_Name == "Tehran" :
+    print("Your university is Sanati Sharif")
+elif city_Name == "Esfahan" :
+    print("Your university is Sanati Esfahan")
+elif city_Name == "Mashhad" :
+    print("Your university is Ferdosi")
+elif city_Name == "Shiraz" :
+    print("Your university is Sanati Shiraz")
+elif city_Name == "Babol" :
+    print("Your university is Anooshiravani Babol")
+else :
+    print("Your city is not valuable")
