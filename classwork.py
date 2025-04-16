e972212c557f7a16a765d7e7b6a2fdfff6bc77cf
mani <manisharifizade84@gmail.com>
2025-04-16 13:48:53 +0330

    Add files via upload

---
+++ classwork.py
@@ -0,0 +1,7 @@
+favorite_color = input("What is your favorite color : ")
+if favorite_color == "Blue":
+    print("Esteghlal")
+elif favorite_color == "Red":
+    print("Perspolis")
+else :
+    print("This color is not available")
